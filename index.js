2db18a50f55fef965449e84ef8b44df544244708
Jakub Šupa <jakub.supa@icloud.com>
2023-01-28 22:37:53 +0100

    add process waiting

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const { log } = require('./app/lib/logger')
 
 const app = {}
 
-app.init = async () => {
-  log('Starting app...')
-  await workers.init()
+app.init = () => {
+  const now = new Date()
+  const delay = 30 - (now.getMinutes() % 30)
+
+  log(`Waiting ${delay} minutes to start...`)
+
+  setTimeout(() => {
+    log('Starting app...')
+
+    workers.init()
+  }, delay * 60 * 1000)
 }
 ;(async () => {
   app.init()
